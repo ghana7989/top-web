0dfb662aa4c2bac00d475f372eee596c0c2a3488
ghana <ghana.chindukuri@gmail.com>
2021-03-13 16:17:50 +0530

    Final commit

---
+++ colors.js
@@ -0,0 +1,11 @@
+const colors = {
+  green: "#3a6351",
+  white: "#f2edd7",
+  orange: "#e48257",
+  black: "#393232"
+}
+
+document.getElementById( "contact-us" ).scrollIntoView( {
+  behavior: "smooth",
+  block: "center"
+} )
